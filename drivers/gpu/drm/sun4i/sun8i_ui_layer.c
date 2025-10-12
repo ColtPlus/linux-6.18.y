259d458abcf848a7032a3e9647dce840dbcb109c
Jernej Skrabec <jernej.skrabec@gmail.com>
2025-10-12 21:23:15 +0200

    drm/sun4i: layer: move num of planes calc out of layer code
    With DE33, number of planes no longer depends on mixer because layers
    are shared between all mixers.
    
    Get this value via parameter, so DE specific code can fill in proper
    value.
    
    Reviewed-by: Chen-Yu Tsai <wens@kernel.org>
    Tested-by: Ryan Walklin <ryan@testtoast.com>
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>

--- drivers/gpu/drm/sun4i/sun8i_ui_layer.c
+++ drivers/gpu/drm/sun4i/sun8i_ui_layer.c
@@ -269,10 +269,10 @@ static const uint64_t sun8i_layer_modifiers[] = {
 struct sun8i_layer *sun8i_ui_layer_init_one(struct drm_device *drm,
 					    struct sun8i_mixer *mixer,
 					    enum drm_plane_type type,
-					    int index)
+					    int index,
+					    int plane_cnt)
 {
 	struct sun8i_layer *layer;
-	unsigned int plane_cnt;
 	int ret;
 
 	layer = devm_kzalloc(drm->dev, sizeof(*layer), GFP_KERNEL);
@@ -294,8 +294,6 @@ struct sun8i_layer *sun8i_ui_layer_init_one(struct drm_device *drm,
 		dev_err(drm->dev, "Couldn't initialize layer\n");
 		return ERR_PTR(ret);
 	}
-
-	plane_cnt = mixer->cfg->ui_num + mixer->cfg->vi_num;
 
 	ret = drm_plane_create_alpha_property(&layer->plane);
 	if (ret) {
